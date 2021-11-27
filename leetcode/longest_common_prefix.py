e6c5868151f39b840c27618d6b70cb7a4c7b3c60
kritika.singhal <kskritikasinghaldev@gmail.com>
2021-11-27 13:28:06 +0530

    sol2: [using zip and set] https://leetcode.com/problems/longest-common-prefix/

--- leetcode/longest_common_prefix.py
+++ leetcode/longest_common_prefix.py
@@ -1,27 +1,23 @@
 class Solution:
-    def longestCommonPrefix(self, strs: list[str]) -> str:
-        pr = strs[0]
-        for s in strs:
-            i = 0
-            while True:
-                if i == len(pr):
-                    break
-                if i == len(s):
-                    pr = s
-                    break
-                if s[i] == pr[i]:
-                    i += 1
-                else:
-                    pr = pr[:i]
-                    break
-        return pr
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = []
+        for x in zip(*strs):
+            if len(set(x)) == 1:
+                prefix.append(x[0])
+            else:
+                break
+        return "".join(prefix)
 
 
 solution = Solution()
-x=int(input())
+x = int(input())
 my_list = []
 while x:
-    x -=1
+    x -= 1
     my_list.append(str(input()))
 
 print(solution.longestCommonPrefix(my_list))
