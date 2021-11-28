57108cea15490b4ff1b3738e1af4fca6a53203b4
kritika.singhal <kskritikasinghaldev@gmail.com>
2021-11-28 14:52:06 +0530

    sol3: [Two pointer] https://leetcode.com/problems/squares-of-a-sorted-array

--- leetcode/square-of-sorted-array.py
+++ leetcode/square-of-sorted-array.py
@@ -1,8 +1,18 @@
 class Solution:
     def sortedSquares(self, nums: list[int]) -> list[int]:
-        squares = [x*x for x in nums]
-        squares.sort()
-        return squares
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
 
 
 solution = Solution()
